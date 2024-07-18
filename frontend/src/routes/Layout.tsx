ec0de839f7df615b18f385d64d3ecc1ef87c1213
Brandon Duong <brandon.duong@outlook.com>
2024-07-18 03:57:21 -0400

    title link to home page

--- frontend/src/routes/Layout.tsx
+++ frontend/src/routes/Layout.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="h-screen w-screen bg-pink-200 text-center p-4">
-      <h1 className="text-4xl font-bold">Anime Eye</h1>
+    <div className="h-screen w-screen bg-pink-200 text-center flex flex-col items-center p-4">
+      <Link to={"/"}>
+        <h1 className="text-4xl font-bold">AInime</h1>
+      </Link>
       <Outlet />
     </div>
   );
