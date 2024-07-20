bf679b320245b44c7560c32ea09667aace81b4d3
Brandon Duong <brandon.duong@outlook.com>
2024-07-20 00:21:54 -0400

    responsive background

--- frontend/src/routes/Layout.tsx
+++ frontend/src/routes/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="h-screen w-screen bg-pink-200 text-center flex flex-col items-center p-4">
+    <div className="h-full w-full bg-pink-200 text-center flex flex-col items-center p-1 md:p-4">
       <Link to={"/"}>
         <h1 className="text-7xl">AInime</h1>
       </Link>
