29fa440da047addbcd4a51147ed00c6fdf03fafa
Brandon Duong <brandon.duong@outlook.com>
2024-07-20 01:35:36 -0400

    handle unknown amount of episodes

--- frontend/src/components/Game/AnimeInfo.tsx
+++ frontend/src/components/Game/AnimeInfo.tsx
@@ -15,14 +15,14 @@ export default function AnimeInfo({ anime }: { anime: AnimeHidden }) {
       <div className="mb-4">
         <div className="md:flex justify-between w-full text-xl font-bold hidden">
           <h3>{anime.type}</h3>
-          <h3>{anime.episodes} Episodes</h3>
+          <h3>{anime.episodes !== null ? anime.episodes : "?"} Episodes</h3>
           <h3>{anime.members} Members</h3>
           <h3>{anime.year}</h3>
         </div>
         <div className="grid grid-cols-2 w-full text-xl font-bold md:hidden">
           <div className="text-start">
             <h3>{anime.type}</h3>
-            <h3>{anime.episodes} Episodes</h3>
+            <h3>{anime.episodes !== null ? anime.episodes : "?"} Episodes</h3>
           </div>
           <div className="text-end">
             <h3>{anime.year}</h3>
--- src/main/java/com/example/demo/services/AnimeService.java
+++ src/main/java/com/example/demo/services/AnimeService.java
@@ -1,7 +1,6 @@
 package com.example.demo.services;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
