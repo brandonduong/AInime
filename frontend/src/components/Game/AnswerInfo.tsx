9ab0165ffc80b49e06c56243e43e87a6ce385cd2
Brandon Duong <brandon.duong@outlook.com>
2024-07-26 03:38:47 -0400

    rating answer

--- frontend/src/components/Game/AnswerInfo.tsx
+++ frontend/src/components/Game/AnswerInfo.tsx
@@ -31,24 +31,66 @@ export default function AnswerInfo({
                 className="h-[200px] w-[140px] md:h-[300px] md:w-[211px]"
               ></div>
             </a>
-            real
-            <h5>
-              <a
-                href={`https://myanimelist.net/${
-                  mode === "title" ? "manga" : "anime"
-                }/${answer.malId}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                MyAnimeList Link
-              </a>
-            </h5>
+            <div>
+              real
+              <h5>
+                <a
+                  href={`https://myanimelist.net/${
+                    mode === "title" ? "manga" : "anime"
+                  }/${answer.malId}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  MyAnimeList Link
+                </a>
+              </h5>
+              <div>
+                <h5>{answer.name}</h5>
+                <h5>fake votes: {answer.aiVotes}</h5>
+                <h5>real votes: {answer.realVotes}</h5>
+              </div>
+            </div>
           </div>
         )}
+      </div>
+    );
+  }
+
+  function RatingAnswer({ answer }: { answer: RatingAnswer }) {
+    return (
+      <div className="flex">
+        <a
+          href={`https://myanimelist.net/${
+            mode === "title" ? "manga" : "anime"
+          }/${answer.malId}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div
+            style={{
+              backgroundImage: `url(${answer.imgUrl.toString()})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+            }}
+            className="h-[200px] w-[140px] md:h-[300px] md:w-[211px]"
+          ></div>
+        </a>
+
         <div>
+          <h5>
+            <a
+              href={`https://myanimelist.net/${
+                mode === "title" ? "manga" : "anime"
+              }/${answer.malId}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              MyAnimeList Link
+            </a>
+          </h5>
           <h5>{answer.name}</h5>
-          <h5>fake votes: {answer.aiVotes}</h5>
-          <h5>real votes: {answer.realVotes}</h5>
+          <h5>{answer.score}</h5>
+          <h5>{answer.scores.join(", ")}</h5>
         </div>
       </div>
     );
@@ -56,7 +98,11 @@ export default function AnswerInfo({
 
   return (
     <div className="border-4 md:p-4 mt-4 border-pink-900">
-      {"fake" in answer ? <AnimeAnswer answer={answer} /> : <div></div>}
+      {"fake" in answer ? (
+        <AnimeAnswer answer={answer} />
+      ) : (
+        <RatingAnswer answer={answer} />
+      )}
     </div>
   );
 }
