2efe7f8fdfe71ef4985c8b22cb2d2139220371a3
Brandon Duong <brandon.duong@outlook.com>
2024-07-25 23:35:14 -0400

    always take 100% space

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import ModeTabs from "../components/Home/ModeTabs";
 export default function Home() {
   return (
-    <div className="flex justify-center overflow-hidden">
+    <div className="flex justify-center overflow-hidden w-full">
       <div className="flex md:basis-2/3 lg:basis-1/2 flex-col">
         <ModeTabs />
         <Outlet />
