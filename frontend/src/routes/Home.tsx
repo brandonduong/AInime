c108226bfff6abdb960b94f28fc10cf290fc4f12
Brandon Duong <brandon.duong@outlook.com>
2024-08-09 19:20:34 -0400

    home button go to same mode

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="flex md:basis-2/3 lg:basis-1/2 flex-col justify-between min-w-0">
         <div className="flex flex-col grow min-h-screen sm:min-h-0 p-1 sm:p-4">
           <div className="flex justify-center relative">
-            <Link to={"/"}>
+            <Link to={mode ? `/${mode}/${getTodayDate()}` : "/"}>
               <h1 className="text-4xl font-black text-pink-900 mb-1">
                 AI<span className="text-pink-400">nime</span>
               </h1>
