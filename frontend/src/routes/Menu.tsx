c0c1a0ca553e4790a3880153ad248ba83d960e12
Brandon Duong <brandon.duong@outlook.com>
2024-08-31 01:53:48 -0400

    loading screen for menu

--- frontend/src/routes/Menu.tsx
+++ frontend/src/routes/Menu.tsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigation } from "react-router-dom";
 import { getTodayDate } from "../common/helper";
 import HomeButton from "../components/Home/HomeButton";
 import Heart from "../components/Icons/Heart";
 import TV from "../components/Icons/TV";
 import Question from "../components/Icons/Question";
+import Loading from "../components/Home/Loading";
 
 export default function Menu() {
+  const { state } = useNavigation();
+  console.log(state);
+
   return (
     <>
       <div className="flex justify-center relative">
@@ -30,58 +34,66 @@ export default function Menu() {
 
       <div className="border-4 border-pink-900 grow overflow-hidden">
         <div className="flex flex-col h-full justify-between overflow-y-auto">
-          <h3 className="flex flex-wrap gap-x-4 justify-between text-lg font-bold text-pink-900 px-4 py-2 bg-pink-300 border-b-4 border-pink-900">
-            <span className="flex gap-1 whitespace-nowrap">Choose Theme:</span>
-          </h3>
-          <div className="p-4 gap-4 grid grow">
-            <Link to={`/ainime`}>
-              <div className="border-4 border-pink-900 h-full">
-                <HomeButton>
-                  <div className="flex justify-center">
-                    <div className="flex-col md:flex-row flex items-center gap-2 md:basis-1/2">
-                      <div className="size-10 min-w-10 min-h-10">
-                        <TV />
-                      </div>
-                      <div className="md:text-start">
-                        <h5>AInime</h5>
-                        <div className="text-sm">
-                          <p className="text-black text-xs italic">
-                            Real vs AI-generated anime plot and manga/light
-                            novel titles
-                          </p>
+          {state !== "loading" ? (
+            <>
+              <h3 className="flex flex-wrap gap-x-4 justify-between text-lg font-bold text-pink-900 px-4 py-2 bg-pink-300 border-b-4 border-pink-900">
+                <span className="flex gap-1 whitespace-nowrap">
+                  Choose Theme:
+                </span>
+              </h3>
+              <div className="p-4 gap-4 grid grow">
+                <Link to={`/ainime`}>
+                  <div className="border-4 border-pink-900 h-full">
+                    <HomeButton>
+                      <div className="flex justify-center">
+                        <div className="flex-col md:flex-row flex items-center gap-2 md:basis-1/2">
+                          <div className="size-10 min-w-10 min-h-10">
+                            <TV />
+                          </div>
+                          <div className="md:text-start">
+                            <h5>AInime</h5>
+                            <div className="text-sm">
+                              <p className="text-black text-xs italic">
+                                Real vs AI-generated anime plot and manga/light
+                                novel titles
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    </HomeButton>
                   </div>
-                </HomeButton>
-              </div>
-            </Link>
+                </Link>
 
-            {[1, 2, 3].map((id) => (
-              <div
-                className="border-4 border-pink-900 grow"
-                key={`theme-${id}`}
-              >
-                <HomeButton disabled>
-                  <div className="flex justify-center">
-                    <div className="flex-col md:flex-row flex items-center gap-2 md:basis-1/2">
-                      <div className="size-10 min-w-10 min-h-10">
-                        <Question />
-                      </div>
-                      <div className="md:text-start">
-                        <h5>???</h5>
-                        <div className="text-sm">
-                          <p className="text-black text-xs italic">
-                            New themes coming soon!
-                          </p>
+                {[1, 2, 3].map((id) => (
+                  <div
+                    className="border-4 border-pink-900 grow"
+                    key={`theme-${id}`}
+                  >
+                    <HomeButton disabled>
+                      <div className="flex justify-center">
+                        <div className="flex-col md:flex-row flex items-center gap-2 md:basis-1/2">
+                          <div className="size-10 min-w-10 min-h-10">
+                            <Question />
+                          </div>
+                          <div className="md:text-start">
+                            <h5>???</h5>
+                            <div className="text-sm">
+                              <p className="text-black text-xs italic">
+                                New themes coming soon!
+                              </p>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    </HomeButton>
                   </div>
-                </HomeButton>
+                ))}
               </div>
-            ))}
-          </div>
+            </>
+          ) : (
+            <Loading />
+          )}
         </div>
       </div>
     </>
