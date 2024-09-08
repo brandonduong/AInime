64b7248f133d09299157f7ae50500022e74ef30e
Brandon Duong <brandon.duong@outlook.com>
2024-09-08 18:43:39 -0400

    move api url to .env

--- frontend/src/api/axiosConfig.tsx
+++ frontend/src/api/axiosConfig.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "https://animai.onrender.com/api";
 export default axios.create({
-  baseURL: "https://animai.onrender.com/api",
+  baseURL: API_BASE_URL,
 });
