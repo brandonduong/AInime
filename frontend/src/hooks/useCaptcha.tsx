8b6ad509113a48cfe931c09fc0e370a8c8310699
Brandon Duong <brandon.duong@outlook.com>
2024-09-10 00:51:15 -0400

    fix grammar

--- frontend/src/hooks/useCaptcha.tsx
+++ frontend/src/hooks/useCaptcha.tsx
@@ -3,6 +3,6 @@ import { CaptchaContext } from "../contexts/captchaContext";
 export function useCaptcha() {
   const context = useContext(CaptchaContext);
   if (!context)
-    throw new Error("useCaptcha must be used within an CaptchaProvider");
+    throw new Error("useCaptcha must be used within a CaptchaProvider");
   return context;
 }
