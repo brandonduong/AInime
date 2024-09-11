ae94ad1fbe92c4fdcbf850ee5e5f5b04d8227cc4
Brandon Duong <brandon.duong@outlook.com>
2024-09-11 00:11:19 -0400

    add darkmode colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-pink-900": "#2e2829",
+        "dark-pink-950": "#191314",
+        "dark-pink-600": "#500724",
+        "dark-pink-500": "#642638",
+        "dark-pink-400": "#783f4e",
+        "dark-pink-300": "#8b5965",
+        "dark-mode": "#443f40",
+      },
+    },
   },
   plugins: [],
   future: {
